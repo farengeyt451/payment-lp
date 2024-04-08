2f7692b7dcf6f0f3e0f3bb11ae29d3435e0ac10e
Alexander Kislov <strannik11.92@gmail.com>
2024-04-08 17:05:06 +0300

    Add button/icon button copmonents

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React, { SyntheticEvent } from 'react';
-import logo from './logo.svg';
+import { SyntheticEvent } from 'react';
 import './App.scss';
-import { Button } from './components/button';
+import DownloadIcon from './assets/icons/arrow-down-circle.svg';
+import { StyledButton } from './components/button';
+import logo from './logo.svg';
+
 function App() {
   function handleButtonAction(e: SyntheticEvent) {
     console.log(e);
@@ -18,7 +20,35 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Button onButtonAction={handleButtonAction}></Button>
+
+        <div
+          style={{
+            display: 'flex',
+          }}
+        >
+          <StyledButton
+            onButtonAction={handleButtonAction}
+            $accent
+            $weight={700}
+          >
+            Log In
+          </StyledButton>
+          <StyledButton
+            onButtonAction={handleButtonAction}
+            $accent
+            $weight={700}
+          >
+            Sign Up
+          </StyledButton>
+          <StyledButton
+            onButtonAction={handleButtonAction}
+            $accent
+            icon={DownloadIcon}
+            $weight={700}
+          >
+            Download
+          </StyledButton>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,15 +1,95 @@
-import { SyntheticEvent } from 'react';
+import { PropsWithChildren, SyntheticEvent } from 'react';
+import { styled } from 'styled-components';
+import * as Colors from '../../styles/colors';
+
 import './button.scss';
 
-function Button({ onButtonAction }: { onButtonAction: (e: SyntheticEvent) => void }) {
+interface ButtonComponent {
+  onButtonAction: (e: SyntheticEvent) => void;
+  className?: string;
+  icon?: string;
+}
+
+function Button({ className, onButtonAction, icon, children }: PropsWithChildren<ButtonComponent>) {
   return (
     <button
-      className="btn"
+      className={className}
       onClick={onButtonAction}
+      style={
+        icon
+          ? {
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }
+          : {}
+      }
     >
-      Login
+      <span
+        style={{
+          display: 'flex',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+        }}
+      >
+        {children}
+        {icon && (
+          <span
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: '22px',
+              height: '22px',
+              marginLeft: '10px',
+            }}
+          >
+            <img
+              src={icon}
+              alt="button-icon"
+              style={{
+                display: 'block',
+                width: '100%',
+                height: '100%',
+              }}
+            />
+          </span>
+        )}
+      </span>
     </button>
   );
 }
 
-export { Button };
+const StyledButton = styled(Button)<{ $accent?: boolean; $weight?: number }>(props => ({
+  position: 'relative',
+  backgroundColor: props.$accent ? Colors.accent : '',
+  fontFamily: 'Poppins, sans-serif',
+  fontSize: '20px',
+  padding: '12px 36px',
+  borderRadius: '10px',
+  border: 'none',
+  cursor: 'pointer',
+  fontWeight: props.$weight || 400,
+}));
+
+// const StyledWithIconButton = styled(StyledButton)<{ $icon: string }>`
+//   padding-right: 58px;
+
+//   &:after {
+//     content: '';
+//     display: block;
+//     position: absolute;
+//     top: calc(50%);
+//     right: 16px;
+//     background-image: url(${DownloadIcon});
+//     background-size: 100%;
+//     z-index: 1;
+//     width: 22px;
+//     height: 22px;
+//     transform: translate(0, -50%);
+//   }
+// `;
+
+export { Button, StyledButton };
